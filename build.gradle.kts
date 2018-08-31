9f18b8a44819c0f1f2699de3083c299ccea72166
Thomas Broyer <t.broyer@ltgt.net>
2018-08-31 11:47:28 +0200

    Update google-java-format plugin and ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("local.maven-publish")
 
     id("net.ltgt.errorprone-javacplugin") version "0.5"
-    id("com.github.sherter.google-java-format") version "0.6"
+    id("com.github.sherter.google-java-format") version "0.7.1"
     id("com.github.hierynomus.license") version "0.14.0"
 }
 
@@ -46,7 +46,7 @@ repositories {
 val ktlint by configurations.creating
 
 dependencies {
-    ktlint("com.github.shyiko:ktlint:0.23.1")
+    ktlint("com.github.shyiko:ktlint:0.27.0")
 }
 
 val verifyKtlint by tasks.creating(JavaExec::class) {
